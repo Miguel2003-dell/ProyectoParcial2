b95c8e186bf5af7bceffec76f420b4ed8ef35e35
Miguel2003-dell <miguelangelmanzanillacastro@gmail.com>
2024-07-03 13:29:22 -0600

    AuthGuard Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,20 +5,21 @@ import { ProductListComponent } from './components/Products/product-list/product
 import { UsuariosListComponent } from './components/Usuarios/usuarios-list/usuarios-list.component';
 import { LoginComponent } from './components/login/login/login.component';
 import { VentasComponent } from './components/Products/ventas/ventas.component';
+import { AuthGuard } from './components/services/guard/auth.guard';
 
 
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'product-list', component: ProductListComponent },
-  { path: 'usuarios-list', component: UsuariosListComponent },
-  { path: 'ventas-list', component: VentasComponent },
+  { path: 'product-list', component: ProductListComponent, canActivate: [AuthGuard] },
+  { path: 'usuarios-list', component: UsuariosListComponent, canActivate: [AuthGuard] },
+  { path: 'ventas-list', component: VentasComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
-
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/components/services/auth/auth.service.ts
+++ src/app/components/services/auth/auth.service.ts
@@ -2,26 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   private apiUrl = 'http://localhost:5000/api/user/login';
+  private isAuthenticated = false;
 
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}`, { username, password })
+    return this.http.post<any>(this.apiUrl, { username, password })
       .pipe(
         tap(response => {
           if (response && response.token) {
             this.setToken(response.token);
-            console.log(response.token)
+            this.isAuthenticated = true;
+            console.log(response.token);
           }
         })
       );
+  }
+
+  isAuthenticatedUser(): boolean {
+    const token = this.getToken();
+    return token !== null && token !== '';
   }
 
   isLoggedIn(): boolean {
@@ -30,6 +36,7 @@ export class AuthService {
 
   logout(): void {
     this.removeToken();
+    this.isAuthenticated = false;
   }
 
   private setToken(token: string): void {
